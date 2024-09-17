e63ed0e39ed007c083c8f311249def12c1f160b9
zoltankiraly19 <zoltan.kiraly@dunaelektronika.com>
2024-09-17 22:38:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import ibm_boto3
+from ibm_botocore.client import Config
+
+def main():
+    # IBM Cloud Object Storage hitelesítési adatok
+    COS_ENDPOINT = "https://s3.us-south.cloud-object-storage.appdomain.cloud"  # Az Object Storage végpontja
+    COS_API_KEY_ID = "cC326fk6u3sRdRsIGE_gyleGM24gynn__WeDyMoqQ52M"  # API kulcs
+    COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/e4dcac9c25e9473485989a3a05ee4ec1:04e94adb-84d3-42d2-ad14-3e0ca6eb4af5::"  # Instance CRN
+
+    bucket_name = "elek-donotdelete-pr-tpbfrekjccp3nj"
+    file_name = "output.txt"
+    text_data = "Ez egy teszt szöveg."
+
+    cos = ibm_boto3.client('s3',
+                           ibm_api_key_id=COS_API_KEY_ID,
+                           ibm_service_instance_id=COS_INSTANCE_CRN,
+                           config=Config(signature_version='oauth'),
+                           endpoint_url=COS_ENDPOINT)
+
+    try:
+        # Fájl feltöltése a bucketbe
+        cos.put_object(
+            Bucket=bucket_name,
+            Key=file_name,
+            Body=text_data
+        )
+        download_url = f"{COS_ENDPOINT}/{bucket_name}/{file_name}"
+        print("Fájl sikeresen feltöltve:", download_url)
+    except Exception as e:
+        print("Hiba történt:", str(e))
+
+if __name__ == "__main__":
+    main()
