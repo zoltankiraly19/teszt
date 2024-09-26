42d5bf2c17ca2e17ff68d6ca5094161e34e3f3df
zoltankiraly19 <zoltan.kiraly@dunaelektronika.com>
2024-09-26 22:50:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import ibm_boto3
 from ibm_botocore.client import Config
 
 app = Flask(__name__)
 
-# Gyökér útvonal ("/") kezelése
+# IBM Cloud Object Storage beállítások
+COS_ENDPOINT = "https://s3.us-south.cloud-object-storage.appdomain.cloud"  # Public endpoint
+COS_API_KEY_ID = "nXkSMheA7GT9zdnrHJwKSqmn-VCJPhXGe7XstbLvrLW2"  # API kulcs
+COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/46c4f2c9bc554c8293a3574f4d0bee64:6208154c-1bfc-437a-8ab5-6c3088afdf56:bucket:elek-donotdelete-pr-flomvcezkdcypu"  # CRN
+bucket_name = "elek-donotdelete-pr-flomvcezkdcypu"  # Bucket neve
+
 @app.route('/')
 def home():
     return "Hello, Flask alkalmazás!"
 
-# IBM Cloud Object Storage beállítások
-COS_ENDPOINT = "https://s3.us-south.cloud-object-storage.appdomain.cloud"  # Public endpoint
-COS_API_KEY_ID = "nXkSMheA7GT9zdnrHJwKSqmn-VCJPhXGe7XstbLvrLW2"  # Új API kulcs
-COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/46c4f2c9bc554c8293a3574f4d0bee64:6208154c-1bfc-437a-8ab5-6c3088afdf56:bucket:elek-donotdelete-pr-flomvcezkdcypu"  # Új CRN
-bucket_name = "elek-donotdelete-pr-flomvcezkdcypu"  # Bucket neve
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file_name = "output111.txt"
-    text_data = "Ez egy teszt szöveg."
+    
+    # Kérjük be a JSON-ből a 'fileContent' értéket
+    text_data = request.json.get('fileContent', 'Alapértelmezett szöveg')  # Ha nincs megadva, 'Alapértelmezett szöveg'
 
     # IBM COS boto3 kliens létrehozása
     cos = ibm_boto3.client('s3',
@@ -28,7 +29,7 @@ def upload_file():
                            endpoint_url=COS_ENDPOINT)
 
     try:
-        # Fájl feltöltése az IBM Cloud Object Storage-ba
+        # Fájl feltöltése az IBM Cloud Object Storage-be
         cos.put_object(
             Bucket=bucket_name,
             Key=file_name,
